c71a2cf5dcd0b400b985156e0068c31e26c6ba54
paulocuambe <pamossecuambe@gmail.com>
2023-03-03 23:57:51 +0200

    a cat like command line tool 03-03-22

---
+++ 03-03-22/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	arguments := os.Args[1:]
+
+	if len(arguments) >= 1 {
+		f, err := os.Open(arguments[0])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b, err := io.ReadAll(f)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(b))
+	} else {
+		log.Fatal("Command not supported")
+	}
+}
