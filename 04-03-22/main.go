54f28a078160e1bfa96bdedbd43a44ebbcd50845
paulocuambe <pamossecuambe@gmail.com>
2023-03-05 00:29:34 +0200

    read config from a yaml file

--- 04-03-22/main.go
+++ 04-03-22/main.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Api struct {
-	Version     string  `json:"version"`
-	Description string  `json:"description"`
-	Name        string  `json:"name"`
-	Author      *Author `json"author"`
-}
-
-func (a *Api) String() string {
-	return a.Name
+	Name        string  `json:"name" yaml:"name"`
+	Version     string  `json:"version" yaml:"version"`
+	Description string  `json:"description" yaml:"description"`
+	Author      *Author `json"author" yaml:"author"`
 }
 
 type Author struct {
-	Name       string `json:"name"`
-	Twitter    string `json:"twitter"`
-	Github     string `json:"github"`
-	Profession string `json:"profession"`
+	Name       string `json:"name" yaml:"name"`
+	Twitter    string `json:"twitter" yaml:"twitter"`
+	Github     string `json:"github" yaml:"github"`
+	Profession string `json:"profession" yaml:"profession"`
 }
 
 func main() {
@@ -37,4 +36,30 @@ func main() {
 
 	log.Printf("%#v", conf)
 	log.Printf("%#v", conf.Author)
+
+	// Reading yaml file
+	log.Printf("\nYaml")
+
+	f2, err := os.Open("api.yml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := io.ReadAll(f2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var conf2 Api
+
+	err = yaml.Unmarshal(b, &conf2)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%#v", conf2)
+	log.Printf("%#v", conf2.Author)
+
 }
