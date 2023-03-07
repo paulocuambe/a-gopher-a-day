32a26cf455ed25327e8492e3e47527b556419154
paulocuambe <pamossecuambe@gmail.com>
2023-03-08 01:19:19 +0200

    format output

--- 07-03-22/main.go
+++ 07-03-22/main.go
@@ -13,31 +13,24 @@ import (
 // what's the user name you want to look up?
 // gosenx
 // Display all the information about the user
-
-// what's the group name you want to look up?
-// root
-// Display all th information about the group
 func main() {
-	// args := os.Args[1:]
-
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Print("What's username you want information about? ")
-	name, err := reader.ReadString('\n')
-
-	name = strings.Trim(name, "\n")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%#v", name)
-
-	u, err := user.Lookup(name)
+	n, err := reader.ReadString('\n')
+	// the returned string will end with \n
+	n = strings.Trim(n, "\n")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%#v", u)
+	u, err := user.Lookup(n)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmp := "User\n\tUid: \t%s \n\tGid: \t%s \n\tName: \t%s \n\tUsername: \t%s \n\tHomeDir: \t%s\n"
+	fmt.Printf(tmp, u.Uid, u.Gid, u.Username, u.Name, u.HomeDir)
 }
