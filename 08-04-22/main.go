e4208208d4fc12c630a0a88c066960181d37f488
paulocuambe <pamossecuambe@gmail.com>
2023-03-09 02:13:53 +0200

    add support for env variable and multiple dirs at once

--- 08-04-22/main.go
+++ 08-04-22/main.go
@@ -5,19 +5,34 @@ import (
 	"log"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 )
 
+var Limit int
+
 func main() {
+	// initialize the global limit
+	dirDepth := os.Getenv("DIR_DEPTH")
+	if l, err := strconv.Atoi(dirDepth); err != nil {
+		Limit = 4
+	} else {
+		Limit = l
+	}
+
 	args := os.Args[1:]
 
 	if len(args) < 2 {
 		log.Fatal("Too few arguments.")
 	}
 
-	
 	if args[0] == "ls" {
-		fmt.Println(printFile(args[1], 1))
+		for _, v := range args[1:] {
+			fmt.Println(printFile(v, 1))
+		}
+
+	} else {
+		fmt.Println("Command not supported. Only [ls] is supported.")
 	}
 }
 
@@ -28,9 +43,18 @@ func printFile(name string, depth int) string {
 		log.Fatal(err)
 	}
 
+	// path.Base only leaves the last part of the path
 	str := fmt.Sprintf("📂 %s\n", path.Base(name))
+
+	// stop recursion if depth is greater than or equal to the limit
+	if depth >= Limit {
+		return str
+	}
+
 	for _, file := range f {
+		// add space
 		space := strings.Repeat("    ", depth)
+
 		inf, err := file.Info()
 
 		if err != nil {
@@ -38,8 +62,10 @@ func printFile(name string, depth int) string {
 		}
 
 		if inf.IsDir() {
-			n := fmt.Sprintf("%s/%s", name, file.Name())
-			str += fmt.Sprintf("%s%s", space, printFile(n, depth + 1))
+			dname := fmt.Sprintf("%s/%s", name, file.Name())
+
+			// recursively print files in the directory
+			str += fmt.Sprintf("%s%s", space, printFile(dname, depth+1))
 		} else {
 			str += fmt.Sprintf("%s📄 %s\n", space, file.Name())
 		}
